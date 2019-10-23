5b84a2bba2e190f8aab6c30da6bee97b7126ba44
RomainMonnoyer <Romain.MONNOYER@student.umons.ac.be>
2019-10-23 15:05:57 +0200

    Exercice 1 tests validés

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,27 @@
 package exercise1;
 
 class Person {
+	private String Name;
+	private int Age;
+
+	//constructeur person
+	public Person(String nom, int monage) {
+		Name = nom;
+		Age = monage;
+	}
+	//fct get var
+	public String getName(){
+		return Name;
+	}
+	public int getAge(){
+		return Age;
+	}
+
+	//fct pour reset var
+	public void setName(String nom){
+		Name=nom;
+	}
+	public void setAge(int monage){
+		Age=monage;
+	}
 }
